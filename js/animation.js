c0b6276a7271e815149a30f6fcd9d49a57b5411c
Elvis Kemoi <elviskemoi001@gmail.com>
2024-05-24 02:29:30 +0300

    Added all service pages. Yet to add specific cards in them

--- js/animation.js
+++ js/animation.js
@@ -1,30 +1,17 @@
-// ScrollReveal({ reset: true, duration: 1000, scale: 0 });
-
-// ScrollReveal().reveal(".logo", {
-// 	reset: false,
-// 	direction: "top",
-// 	distance: "200px",
-// 	scale: "3",
-// });
-
-// ScrollReveal().reveal(".carousel-item");
 ScrollReveal().reveal(".box", {
 	easing: "ease-in-out",
-
-	reset: true,
+	reset: !0,
 	delay: "50",
 	direction: "top",
 	origin: "bottom",
 	duration: 800,
-	distance: "100px",
-});
-ScrollReveal().reveal(".icons", {
-	// easing: "ease-in-out",
-
-	reset: true,
-	delay: "50",
-	direction: "right",
-	origin: "bottom",
-	duration: 800,
-	distance: "100px",
-});
+	distance: "300px",
+}),
+	ScrollReveal().reveal(".icons", {
+		reset: !0,
+		delay: "50",
+		direction: "right",
+		origin: "bottom",
+		duration: 800,
+		distance: "100px",
+	});
